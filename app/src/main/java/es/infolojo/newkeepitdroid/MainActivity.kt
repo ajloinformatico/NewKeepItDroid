a39854dfac1f13b06e569d2fdb8633e36d2b2232
Antonio José Lojo Ojeda <57002879+ajloinformatico@users.noreply.github.com>
2025-02-17 17:54:37 +0100

    set upp hilt with the app

--- app/src/main/java/es/infolojo/newkeepitdroid/MainActivity.kt
+++ app/src/main/java/es/infolojo/newkeepitdroid/MainActivity.kt
@@ -5,16 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import es.infolojo.newkeepitdroid.ui.screens.home.HomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 import es.infolojo.newkeepitdroid.ui.screens.search.SearchScreen
 import es.infolojo.newkeepitdroid.ui.theme.NewKeepItDroidTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/es/infolojo/newkeepitdroid/di/AppModule.kt
@@ -0,0 +1,11 @@
+package es.infolojo.newkeepitdroid.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    // OBJECTS TO PROVIDE
+}
