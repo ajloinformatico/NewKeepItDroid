4490aef589f2ef0f9bf12ba09cd2d1c660d2a4d3
Antonio José Lojo Ojeda <57002879+ajloinformatico@users.noreply.github.com>
2025-04-13 13:30:20 +0200

    remove not needed import

--- app/src/main/java/es/infolojo/newkeepitdroid/ui/screens/update/UpdateScreen.kt
+++ app/src/main/java/es/infolojo/newkeepitdroid/ui/screens/update/UpdateScreen.kt
@@ -3,7 +3,6 @@ package es.infolojo.newkeepitdroid.ui.screens.update
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
